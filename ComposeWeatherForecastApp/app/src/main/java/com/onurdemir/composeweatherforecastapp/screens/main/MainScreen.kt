b4b0326b24de679bbcc46b5836b2fd415c70eedd
onurdemir43 <142926729+onurdemir43@users.noreply.github.com>
2023-11-18 15:01:55 +0300

    Setting up the AppBar

--- ComposeWeatherForecastApp/app/src/main/java/com/onurdemir/composeweatherforecastapp/screens/main/MainScreen.kt
+++ ComposeWeatherForecastApp/app/src/main/java/com/onurdemir/composeweatherforecastapp/screens/main/MainScreen.kt
@@ -1,33 +1,66 @@
 package com.onurdemir.composeweatherforecastapp.screens.main
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.onurdemir.composeweatherforecastapp.data.DataOrException
 import com.onurdemir.composeweatherforecastapp.model.Weather
+import com.onurdemir.composeweatherforecastapp.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController,
                mainViewModel: MainViewModel = hiltViewModel()) {
-    ShowData(mainViewModel)
-}
 
-@SuppressLint("ProduceStateDoesNotAssignValue")
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)) {
-    value = mainViewModel.getWeatherData("Eskişehir")
+        value = mainViewModel.getWeatherData("Eskişehir")
     }.value
 
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     }else if(weatherData.data != null) {
-        Text(text = "MainScreen ${weatherData.data.toString()}")
+        MainScaffold(weather = weatherData.data!!, navController)
+
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("ProduceStateDoesNotAssignValue", "UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    
+    Scaffold(topBar = {
+        WeatherAppBar(title = weather.city.name + ",${weather.city.country}",
+        icon = Icons.Default.ArrowBack,
+            navController = navController){
+            Log.d("TAG", "MainScaffold: Button Clicked")
+        }
+    }) {
+        Surface(modifier = Modifier.padding(100.dp)) {
+            MainContent(data = weather)
+        }
+
+    }
+
+
+}
+
+@Composable
+fun MainContent(data: Weather) {
+    Text(text = data.city.name)
 
 }
---
+++ ComposeWeatherForecastApp/app/src/main/java/com/onurdemir/composeweatherforecastapp/widgets/WeatherAppBar.kt
@@ -0,0 +1,87 @@
+package com.onurdemir.composeweatherforecastapp.widgets
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WeatherAppBar(
+    title: String = "Title",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    //elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {} ) {
+
+    TopAppBar(title = {
+        Text(text = title,
+            color = Color.Blue,
+            style = TextStyle(fontWeight = FontWeight.Bold,
+                fontSize = 15.sp),
+        )
+    },
+        actions = {
+                  if (isMainScreen) {
+                      IconButton(onClick = { /*TODO*/ }) {
+                          Icon(
+                              imageVector = Icons.Default.Search,
+                              contentDescription = "search icon"
+                          )
+                      }
+                      IconButton(onClick = { /*TODO*/ }) {
+                          Icon(
+                              imageVector = Icons.Rounded.MoreVert,
+                              contentDescription = "more icon"
+                          )
+                      }
+                  }else {
+                      Box{}
+                  }
+        },
+        navigationIcon = {
+                         if (icon != null) {
+                             Icon(imageVector = icon, contentDescription = null,
+                             modifier = Modifier.clickable {
+                                 onButtonClicked.invoke()
+                             })
+                         }
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Transparent))
+
+
+
+
+}
+
+
+
+
+
+
+
